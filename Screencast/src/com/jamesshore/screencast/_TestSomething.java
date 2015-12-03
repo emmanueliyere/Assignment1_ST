8a38d3b70d4ccd66c03b2f85bf293a2b541f6ce4
emmanueliyere <ei222br@student.lnu.se>
2015-12-03 17:58:11 +0100

    Test1:
    First test, was implemented to be sure that the test code works, it was
    first set to fail and when edited, to pass

--- Screencast/src/com/jamesshore/screencast/_TestSomething.java
+++ Screencast/src/com/jamesshore/screencast/_TestSomething.java
@@ -5,10 +5,25 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class _TestSomething {
+	
+	/*
+	 * Test1: First test
+	 */
 
 	@Test
-	public void testNothing() {
+	public void nothing() {
 		
+	}
+	/*
+	 * Test2: is to tract the transaction of a Saving account, 
+	 * such as: it deposit
+	 */
+	@Test
+	public void savingsAccount(){
+		SavingsAccount account = new SavingsAccount();
+		account.deposit(100);
+		assertEquals(100, account.balance());
 	}
 
 }
+
